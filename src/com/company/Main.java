7e2c5ef0ca2046f3341f28f9e6965f13bc0aa387
Zhakshylyk <zhakshylykk@gmail.com>
2024-09-14 16:12:26 +0600

    распечатываю Person

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Давайте поработаем с гитом для начала");
+
+        Person person = new Person("Sasha", 30);
+        System.out.println(person);
     }
 }
